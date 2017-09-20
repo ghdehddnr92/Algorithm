23a8ee948afa79a1d45f1f3693f12f01eab1efa9
ghdhddnr92 <ghdehddnr92@gmail.com>
2017-09-20 12:30:18 +0900

    Algo_1085

--- src/Algo_1085.java
+++ src/Algo_1085.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 
 public class Algo_1085 {
-
+	static int x,y,w,h;
+	public static void main(String[]args){
+		Scanner in= new Scanner(System.in);
+		
+		x= in.nextInt();
+		y=in.nextInt();
+		w=in.nextInt();
+		h = in.nextInt();
+		
+	
+		
+		System.out.println(Math.min(Math.min(Math.min(x, y),w-x),h-y));
+	}
 }
