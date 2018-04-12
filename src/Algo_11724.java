f264cc9c5de66304e9f491865c313921a22e8bb2
ghdhddnr92 <ghdehddnr92@gmail.com>
2018-04-12 12:54:27 +0900

    Sw_4155

--- src/Algo_11724.java
+++ src/Algo_11724.java
@@ -2,11 +2,46 @@ import java.util.Scanner;
 
 public class Algo_11724 {
 	static int N,M;
+	static int arr[][];
+	static boolean visited[];
 	public static void main(String[]args){
 		Scanner in = new Scanner(System.in);
-		
+		N = in.nextInt();
+		M = in.nextInt();
+		arr= new int[N+1][N+1];
+		visited = new boolean[N+1];
 		for(int i=0;i<M;i++){
-			
+			int y = in.nextInt();
+			int x = in.nextInt();
+			arr[y][x] = arr[x][y]= 1;
+		}
+		//showArr();
+		int cnt =0;
+		for(int i=1;i<=N;i++){
+			if(!visited[i]){
+				dfs(i);
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+	public static void dfs(int x){
+		visited[x] = true;
+		
+		for(int j=1;j<=N;j++){
+			if(arr[x][j]==1 && !visited[j]){
+			//	System.out.println(x+" "+j+"로 이동 ");
+				dfs(j);
+			}
+		}
+	}
+	
+	public static void showArr(){
+		for(int i=1;i<=N;i++){
+			for(int j=1;j<=N;j++){
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println("");
 		}
 	}
 }
---
+++ src/Sw_4155.java
@@ -0,0 +1,111 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Sw_4155 {
+	static int T;
+	static int N,M;
+	static boolean visited[][][];
+	static String arr[][];
+	static int dy[] = {1,0,-1,0};
+	static int dx[] = {0,1,0,-1};
+	static int res = -1;
+	public static void main(String[]args) throws NumberFormatException, IOException{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+		T = Integer.parseInt(br.readLine());
+		StringTokenizer st; 
+		for(int t=1;t<=T;t++){
+			st = new StringTokenizer(br.readLine());
+
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			arr = new String[N][M];
+			visited = new boolean[N][M][1<<5+1];
+
+			for(int i=0;i<N;i++){
+				String line = br.readLine();
+				for(int j=0;j<M;j++){
+					arr[i][j] = line.charAt(j)+"";
+				}
+			}
+
+			solve();
+			System.out.println("#"+t+" "+res);
+			res =-1;
+		}
+	}
+	public static void solve(){
+		for(int i=0;i<N;i++){
+			for(int j=0;j<M;j++){
+				if(arr[i][j].equals("0")){
+					bfs(i,j);
+				}
+			}
+		}
+	}
+	public static void bfs(int y,int x){
+		Queue<OnePiece> q= new LinkedList<OnePiece>();
+		q.add(new OnePiece(y,x,0));
+		visited[y][x][0] = true;
+		int time = 1;
+		while(!q.isEmpty()){
+			int qSize = q.size();
+
+			for(int size = 0;size<qSize;size++){
+				OnePiece tmp = q.poll();
+
+				for(int i=0;i<4;i++){
+					int ny = tmp.y+dy[i];
+					int nx = tmp.x+dx[i];
+					int nKey = tmp.key;
+					
+					if(ny<0 || nx< 0 || ny>=N || nx>=M){
+						continue;
+					}
+					System.out.println(ny+" "+nx+" "+nKey);
+					if(visited[ny][nx][nKey]){
+				//		System.out.println("이미 방문 ");
+						continue;
+					}
+					if(arr[ny][nx].equals("#")){
+					//	System.out.println("# 이어서 못감");
+						continue;
+					}
+					if(arr[ny][nx].equals("a")||arr[ny][nx].equals("b")
+							||arr[ny][nx].equals("c")||arr[ny][nx].equals("d")
+							||arr[ny][nx].equals("e")||arr[ny][nx].equals("f")){
+						nKey |= (1<<arr[ny][nx].charAt(0)-'a');
+					}
+					if(arr[ny][nx].equals("A")||arr[ny][nx].equals("B")
+							||arr[ny][nx].equals("C")||arr[ny][nx].equals("D")
+							||arr[ny][nx].equals("E")||arr[ny][nx].equals("F")){
+						if((nKey & (1<<arr[ny][nx].charAt(0)-'A'))<=0){
+							continue;
+						}	
+					}
+					if(arr[ny][nx].equals("1")){
+						res = time;
+						return;
+					}
+					q.add(new OnePiece(ny,nx,nKey));
+					visited[ny][nx][nKey] = true;
+				}
+			}
+			time++;	
+		}
+	}
+}
+class OnePiece{
+	int y;
+	int x;
+	int key;
+	OnePiece(int y, int x,int key){
+		this.y = y;
+		this.x = x;
+		this.key = key;
+	}
+}
